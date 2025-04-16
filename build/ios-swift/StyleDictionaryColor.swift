10cbea1f1d3ed16c226cad9c8cbb6a6a8619943a
Giovani Nícolas Bettoni <gbettoni@192.168.0.99>
2025-04-16 12:08:49 -0300

    ios created when all tokens had attributes

---
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -0,0 +1,23 @@
+
+//
+// StyleDictionaryColor.swift
+//
+
+// Do not edit directly, this file was auto-generated.
+
+
+import UIKit
+
+public class {
+    public static let colorsBlack = #000000
+    public static let colorsOrange100 = #fffaf0
+    public static let colorsOrange200 = #feebc8
+    public static let colorsOrange300 = #fbd38d
+    public static let colorsOrange400 = #f6ad55
+    public static let colorsOrange500 = #ed8936
+    public static let colorsOrange600 = #dd6b20
+    public static let colorsOrange700 = #c05621
+    public static let colorsOrange800 = #9c4221
+    public static let colorsOrange900 = #7b341e
+    public static let colorsWhite = #ffffff
+}
